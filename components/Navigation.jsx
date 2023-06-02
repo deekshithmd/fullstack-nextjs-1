c67c256a1546b75c1375791b87c6c7b9d4e56991
deekshithmd <deekshithmogra@gmail.com>
2023-06-02 22:24:10 +0530

    consumed api

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,3 +1,14 @@
+"use client";
+import axios from "axios";
 export const Navigation = () => {
-  return <h1>Navigation</h1>;
+  const getData = async () => {
+    const res = await axios.get("http://localhost:3000/api");
+    console.log("Response", res);
+  };
+  return (
+    <div>
+      <h1>Navigation</h1>
+      <button onClick={getData}>load</button>
+    </div>
+  );
 };
